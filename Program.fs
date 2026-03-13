938ac076552768e69045fd84866ccec71e50e162
kex1t <56569962+k3xit@users.noreply.github.com>
2026-03-13 10:13:53 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+open System.IO
+
+let printSeq inputSeq = 
+    for x in inputSeq do
+        printfn "\t%s" x
+
+let fullFile dir =
+    Directory.EnumerateFiles(dir, "*.txt")
+let fullDir dir =
+    Directory.EnumerateDirectories(dir)
+let rec searchDir () =
+    printf "Введите путь каталога: "
+    let dir = string(Console.ReadLine())
+    match dir with
+    | "" -> 
+        printfn "Выход"
+    | _ ->
+        match Directory.Exists(dir) with 
+        | true -> 
+            let aDir = fullDir dir
+            for bDir in aDir do 
+                searchFile bDir
+        | false -> 
+            printfn "Нет такой директории"
+            searchDir ()
+
+and searchFile dir =
+    let fSeq = fullFile dir
+    match fSeq |> Seq.isEmpty with 
+    | true -> 
+        printfn "Директория: %s" dir
+        printfn "Нет текстовых файлов"
+    | false ->
+        printfn "Директория: %s" dir
+        printfn "Пути к текстовым файлам: "
+        printSeq fSeq
+
+[<EntryPoint>]
+let main args =
+    searchDir ()
+    0
