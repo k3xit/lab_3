6fe5c8df6b666eed3357a3eec9245c7e1fa13fd8
kex1t <56569962+k3xit@users.noreply.github.com>
2026-03-13 10:13:01 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+
+let rec inputInt () = 
+    match System.Int32.TryParse(Console.ReadLine()) with
+    | true, convertInt -> convertInt
+    | _ -> 
+        printfn "Ошибка, повторите ввод\n>"
+        inputInt ()
+
+let seqInput seqLen =
+    seq {
+        for i in 1 .. seqLen do
+            printf "%i. Введите целое число: " i
+            match System.Int32.TryParse(Console.ReadLine()) with
+            | true, convertInt -> yield convertInt
+            | _ -> 
+                printfn "Ошибка, повторите ввод"
+    }
+
+[<EntryPoint>]
+let main args =
+    printfn "Создание изначальной последовательности"
+    printf "Введите количество элементов последовательности: "
+    let seqLen = inputInt ()
+    let aSeq = seqInput seqLen
+    printf "\nЧисло для поиска: "
+    let a = inputInt ()
+    let b = aSeq |> Seq.fold(fun acc elem -> 
+                            if elem=a then
+                                acc+1
+                            else
+                                acc
+                        ) 0 
+    printfn "Количество совпадений: %i" b
+    0
